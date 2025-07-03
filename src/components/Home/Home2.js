e321ceb19433621b84d2a8fe0063386ea3739224
hasti <hasti6@movaliya.com>
2025-07-03 16:59:50 +0530

    url

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -50,7 +50,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/hastimovaliya"
+                  href="https://github.com/hastimovaliya55"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
